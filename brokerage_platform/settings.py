14daaa2600cbabd1511cf941c0e238223e14561c
Batman <itchysurfer@gmail.com>
2026-04-15 12:41:31 +0100

    Improve responsive UI, update layout scaling, and add broker testimonials section

--- brokerage_platform/settings.py
+++ brokerage_platform/settings.py
@@ -188,9 +188,10 @@ WSGI_APPLICATION = 'brokerage_platform.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
